ea22bf93932c0709353a26db6b91f2fbf4c460b5
Lucas Claudino <lucasclaudino9@gmail.com>
2024-06-01 12:49:48 -0300

    "Refactore validators and add loading"

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -9,6 +9,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { CommonModule } from '@angular/common';
+import { MatSpinner } from '@angular/material/progress-spinner';
 
 
 @Component({
@@ -21,18 +22,31 @@ import { CommonModule } from '@angular/common';
     MatIconModule,
     MatGridListModule,
     ReactiveFormsModule,
-    CommonModule
+    CommonModule,
+    MatSpinner
   ],
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent  implements OnInit {
+export class LoginComponent implements OnInit {
+  // Variáveis definidas no início do componente
   loginForm: FormGroup;
   hide = true;
   submitted = false;
-  public emailErrorMessage: string | undefined;
-  public passwordErrorMessage: string | undefined;
+  invalidCredentials = false;
+  emailErrorMessage: string | undefined;
+  passwordErrorMessage: string | undefined;
+  invalidCrendentialsText: boolean = false;
+  isLoading: boolean = false;
+
+
+
+  private readonly EMAIL_REQUIRED_MESSAGE = 'Por favor digite seu email cadastrado (email de cadastro)';
+  private readonly EMAIL_INVALID_MESSAGE = 'Digite um email valido';
+  private readonly PASSWORD_REQUIRED_MESSAGE = 'Digite uma senha';
+
+  private readonly EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   constructor(
     private fb: FormBuilder,
@@ -41,25 +55,18 @@ export class LoginComponent  implements OnInit {
     private changeDetector: ChangeDetectorRef
   ) {
     this.loginForm = this.fb.group({
-      email: [''],
-      password: [''],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
     });
 
-    this.loginForm.get('email')?.valueChanges.subscribe(() => {
-      if (this.submitted) {
-        this.validateForm();
-      }
-    });
-  
-    this.loginForm.get('password')?.valueChanges.subscribe(() => {
+    this.loginForm.valueChanges.subscribe(() => {
       if (this.submitted) {
         this.validateForm();
       }
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   get email() {
     return this.loginForm.get('email');
@@ -69,67 +76,70 @@ export class LoginComponent  implements OnInit {
     return this.loginForm.get('password');
   }
 
-  validateForm(emailErrorMessage?: string): void {
+  validateForm(): void {
     this.submitted = true;
-  
-    const emailControl = this.loginForm.get('email');
-    const passwordControl = this.loginForm.get('password');
-    let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
+    const emailControl = this.email;
+    const passwordControl = this.password;
 
-  
-    if (emailControl) {
-      let isValidEmail = emailRegex.test(emailControl.value);
-
-      emailControl.setValidators([Validators.required, Validators.email]);
-  
-      if (!emailControl.value) {
-        this.emailErrorMessage = 'Por favor digite seu email cadastrado (email de cadastro)';
-      } else if (!isValidEmail) {
+    if (this.invalidCredentials) {
+      this.invalidCrendentialsText = true
+      if (emailControl) {
         emailControl.setErrors({ 'invalid': true });
-        console.log('Email inválido');
-        this.emailErrorMessage = 'Digite um email valido';
+      }
+      if (passwordControl) {
+        passwordControl.setErrors({ 'invalid': true });
+      }
+      this.invalidCredentials = false;
+    }
+
+    if (emailControl) {
+      const isValidEmail = this.EMAIL_REGEX.test(emailControl.value);
+
+      if (emailControl.invalid && emailControl.errors?.['required']) {
+        this.emailErrorMessage = this.EMAIL_REQUIRED_MESSAGE;
+      } else if (!isValidEmail) {
+        this.emailErrorMessage = this.EMAIL_INVALID_MESSAGE;
+        emailControl.setErrors({ 'invalid': true });
       } else {
         this.emailErrorMessage = '';
       }
     }
-  
-    this.loginForm.markAllAsTouched();
-  
+
     if (passwordControl) {
-        if (!passwordControl.value) {
-            console.log('Senha inválida');
-            passwordControl.setErrors({ 'invalid': true });
-            this.passwordErrorMessage = 'Digite uma senha';
-        } else {
-            passwordControl.clearValidators();
-            this.passwordErrorMessage = '';
-        }
+      if (passwordControl.invalid && passwordControl.errors?.['required']) {
+        this.passwordErrorMessage = this.PASSWORD_REQUIRED_MESSAGE;
+      } else {
+        this.passwordErrorMessage = '';
+      }
     }
+
+    this.loginForm.markAllAsTouched();
   }
-  
 
   login(): void {
-      const emailValue = this.loginForm.get('email')?.value;
-      const passwordValue = this.loginForm.get('password')?.value;
+    this.validateForm();
 
-      if (!emailValue || !passwordValue) {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.isLoading = true;
+
+    this.authService.login(this.loginForm.value).subscribe(
+      (response: { token: string, error?: string }) => {
+        this.isLoading = true;
+        if (response.token) {
+          this.router.navigate(['/home']);
+        } else {
+          this.invalidCredentials = true;
           this.validateForm();
-          return;
-      } else {
-          console.log("Formulário enviado");
-          this.authService.login(this.loginForm.value).subscribe(
-              (response: { token: string, error?: string }) => {
-                  if (response.token) {
-                      console.log('Login bem-sucedido', response);
-                      this.router.navigate(['/home']);
-                  } else {
-                      console.error('Login falhou');
-                      console.log('Resposta', response);
-                      this.validateForm(response.error);
-                  }
-              }
-          );
+        }
+      },
+      (error: any) => {
+        this.isLoading = false;
+        console.error('Erro no login', error);
       }
+    );
   }
 }
